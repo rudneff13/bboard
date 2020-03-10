335985531ecdfda6c7de61021dc184cb0a00fc4d
Dmytro Rudniev <rudneff13@gmail.com>
2020-03-10 22:35:42 +0200

    added authorization

---
+++ main/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import index, other_page, BBLoginView, profile, BBLogoutView, ChangeUserInfoView, BBPasswordChangeView, \
+    RegisterDoneView, RegisterUserView, user_activate, DeleteUserView
+
+app_name = 'main'
+urlpatterns = [
+    path('accounts/register/activate/<str:sign>/', user_activate, name='register_activate'),
+    path('accounts/register/done/', RegisterDoneView.as_view(), name='register_done'),
+    path('accounts/register/', RegisterUserView.as_view(), name='register'),
+    path('accounts/login/', BBLoginView.as_view(), name='login'),
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
+    path('accounts/profile/delete/', DeleteUserView.as_view(), name='profile_delete'),
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/password_change/', BBPasswordChangeView.as_view(), name='password_change'),
+    path('<str:page>/', other_page, name='other'),
+    path('', index, name='index'),
+]
